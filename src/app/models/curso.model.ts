6b519eceb4882831845de06f33720879642118c7
AlexLimaValle <alexlimavalle@gmail.com>
2023-10-16 23:32:00 -0300

    subidad de mejoras

--- src/app/models/curso.model.ts
+++ src/app/models/curso.model.ts
@@ -1,10 +1,11 @@
 import { Tema } from '../models/tema.model';
+import { Docente } from "../models/docente.model";
 
 export class Curso {
 	id?: number;
 	nombre?: string;	
 	tema?: Tema;
 	fechaInicio?: Date;
-	idDocente?: number;
+	idDocente?: Number;
 }
 
